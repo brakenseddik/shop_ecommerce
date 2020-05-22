2b20bb517bfdbd2eb3f552d51ffa046411157039
Seddik <dzmidou95@gmail.com>
2020-05-22 16:35:13 +0000

    horizontal list view

---
+++ lib/components/horizontal_listview.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class horizontalListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Category(
+            image: 'images/cats/tshirt.png',
+            caption: 'T-shirt',
+          ),
+          Category(
+            image: 'images/cats/jeans.png',
+            caption: 'Jeans',
+          ),
+          Category(
+            image: 'images/cats/shoe.png',
+            caption: 'Shoes',
+          ),
+          Category(
+            image: 'images/cats/informal.png',
+            caption: 'Informal',
+          ),
+          Category(
+            image: 'images/cats/dress.png',
+            caption: 'Dresses',
+          ),
+          Category(
+            image: 'images/cats/formal.png',
+            caption: 'Formal',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String image;
+  final String caption;
+  Category({this.image, this.caption});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.amberAccent.shade100,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          //width: 100.0,
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                image,
+                width: 100,
+                height: 70,
+              ),
+              Text(caption),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:shoppy/components/horizontal_listview.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,8 +17,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget carousel = Container(
+      height: 200,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotColor: Colors.amber,
+        autoplay: false,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+        ],
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.amber,
         title: Text('Shoppy'),
         actions: <Widget>[
@@ -53,9 +74,65 @@ class _HomePageState extends State<HomePage> {
               accountName: Text('Braken Mohammed'),
             ),
             //Body
-            ListTile(title: Text('Homepage'), leading: Icon(Icons.home))
+            InkWell(
+                child: ListTile(
+                    title: Text('Homepage'),
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.amber,
+                    ))),
+            InkWell(
+                child: ListTile(
+                    title: Text('My Account'),
+                    leading: Icon(
+                      Icons.person,
+                      color: Colors.amber,
+                    ))),
+            InkWell(
+                child: ListTile(
+                    title: Text('My Orders'),
+                    leading: Icon(
+                      Icons.shopping_basket,
+                      color: Colors.amber,
+                    ))),
+            InkWell(
+                child: ListTile(
+                    title: Text('Categories'),
+                    leading: Icon(
+                      Icons.dashboard,
+                      color: Colors.amber,
+                    ))),
+            InkWell(
+                child: ListTile(
+                    title: Text('Favorites'),
+                    leading: Icon(
+                      Icons.favorite,
+                      color: Colors.amber,
+                    ))),
+            Divider(),
+            InkWell(
+                child: ListTile(
+                    title: Text('Settings'), leading: Icon(Icons.settings))),
+            InkWell(
+                child: ListTile(
+                    title: Text('About'),
+                    leading: Icon(
+                      Icons.help,
+                      color: Colors.green,
+                    ))),
           ],
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          carousel,
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Categories'),
+          ),
+          //horizontal  LISTVIEW
+          horizontalListView(),
+        ],
       ),
     );
   }
